a0b1a81297512f7b80d7212729d55a7966f6d0d8
soardev <soardev>
2023-11-14 18:07:44 +0000

    xxx

--- Virus Analysis.py
+++ Virus Analysis.py
@@ -12,9 +12,43 @@ from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
 
-
+    # call 'url_reputation_1' block
+    url_reputation_1(container=container)
 
     return
+
+@phantom.playbook_block()
+def url_reputation_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("url_reputation_1() called")
+
+    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.cef.requestURL","artifact:*.id"])
+
+    parameters = []
+
+    # build parameters list for 'url_reputation_1' call
+    for container_artifact_item in container_artifact_data:
+        if container_artifact_item[0] is not None:
+            parameters.append({
+                "url": container_artifact_item[0],
+                "context": {'artifact_id': container_artifact_item[1]},
+            })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.act("url reputation", parameters=parameters, name="url_reputation_1", assets=["phishtank"])
+
+    return
+
 
 @phantom.playbook_block()
 def on_finish(container, summary):
